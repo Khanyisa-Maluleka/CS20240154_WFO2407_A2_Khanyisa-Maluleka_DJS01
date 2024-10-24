cd1ef724fd4dbb97ada74dd9db15a8ef90557101
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-10-25 00:00:17 +0200

    Converted fuel burn rate from (kg/s) to (kg/h)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const acceleration = 3 * 12960; // converted acceleration (m/s^2) to (km/h^2)
 const time = 1; // converted time from seconds to hours
 const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5 * 3600; // fuel burn rate (kg/s)
+const fbr = 0.5 * 3600; // converted fuel burn rate from (kg/s) to (kg/h)
 
 
 const newDistance = distance + (velocity*time) //calcultes new distance
@@ -26,6 +26,8 @@ function calcNewVel (velocity, acceleration, time) {
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
 console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+
+
 
 
 

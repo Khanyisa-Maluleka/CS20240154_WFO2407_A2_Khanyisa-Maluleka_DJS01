bb365463ad45a6bdbe59c6c6a34b1c86ac1ab91d
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-10-25 10:14:29 +0200

    Fixed the remaining fuel formula

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const velocity = 10000; // velocity (km/h)
 const acceleration = 3 * 12960; // converted acceleration (m/s^2) to (km/h^2)
 const time = 1; // converted time from seconds to hours
 const distance = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5 * 3600; // converted fuel burn rate from (kg/s) to (kg/h)
+const initialFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5 * 3600; // converted fuel burn rate from (kg/s) to (kg/h)
 
 
 const newDistance = distance + (velocity*time) //calcultes new distance
-const remainingFuel = fbr*time //calculates remaining fuel
+const remainingFuel = initialFuel - fuelBurnRate*time //calculates remaining fuel
 const newVelocity = calcNewVel(acceleration, velocity, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
@@ -26,6 +26,7 @@ function calcNewVel (velocity, acceleration, time) {
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
 console.log(`Corrected New Distance: ${newDistance} km`);
 console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+
 
 
 
